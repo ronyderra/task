50f2cdc0b632d93ca0f734330ee543800c71df77
rony derra <derrarony@gmail.com>
2023-03-07 19:02:38 +0200

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Game from "./pages/game";
+
+function App() {
+  return (
+    <div className="tic-tac-toe">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/PopUp.jsx
@@ -0,0 +1,63 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+const PopUp = ({ title, body, btnText, btnFunc }) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={"parent-box"}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="winner"
+      >
+        <motion.div
+          key={"child-box"}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          exit={{ scale: 0, opacity: 0 }}
+          className="text"
+        >
+          <motion.h2
+            initial={{ scale: 0, y: 100 }}
+            animate={{
+              scale: 1,
+              y: 0,
+              transition: {
+                y: { delay: 0.7 },
+                duration: 0.7,
+              },
+            }}
+          >
+            {title}
+          </motion.h2>
+
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{
+              scale: 1,
+              transition: {
+                delay: 1.3,
+                duration: 0.2,
+              },
+            }}
+            className="win"
+          >
+            {body}
+          </motion.div>
+
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{
+              scale: 1,
+              transition: { delay: 1.5, duration: 0.3 },
+            }}
+          >
+            <button onClick={() => btnFunc()}>{btnText}</button>;
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default PopUp;
