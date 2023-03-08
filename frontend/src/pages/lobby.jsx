4e7ccaa596c79f7d403271966670c5830366871b
rony derra <derrarony@gmail.com>
2023-03-09 00:31:37 +0200

    Lobby

--- frontend/src/pages/lobby.jsx
+++ frontend/src/pages/lobby.jsx
@@ -1,11 +1,14 @@
 import LobbyBackg from "../components/lobby/LobbyBackg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PopUp from "../components/PopUp";
 import { useNavigate } from "react-router-dom";
 
 function Lobby() {
-  const [match, setMatch] = useState(true);
+  const [match, setMatch] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {}, []);
+
   return match ? (
     <PopUp
       title={"Lets PLay"}
