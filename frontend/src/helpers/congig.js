b82b30542550580c5a9839d989c957eb03cdbc0c
rony derra <derrarony@gmail.com>
2023-03-10 01:30:46 +0200

    socket

--- frontend/src/helpers/congig.js
+++ frontend/src/helpers/congig.js
@@ -3,4 +3,4 @@ export const config = {
   _DEFAULT_SERVER: "http://localhost:3030",
   _DEFAULT_SOCKET: "wss://localhost:3030",
 };
-export const socket = new Socket("http://localhost:3030");
+export const socket = new Socket(config._DEFAULT_SERVER);
