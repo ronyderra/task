7d84fe93f07b1603d1c9cabea078c0c86a744a9e
ronyderra <derrarony@gmail.com>
2023-04-24 16:12:02 +0300

    config

--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,6 +1,6 @@
 import Socket from "./socket";
 export const config = {
-  _DEFAULT_SERVER: "http://localhost:3030",
-  _DEFAULT_SOCKET: "wss://localhost:3030",
+  _DEFAULT_SERVER: "http://16.16.123.66:3030",
+  _DEFAULT_SOCKET: "wss://16.16.123.66:3030",
 };
 export const socket = new Socket(config._DEFAULT_SERVER);
